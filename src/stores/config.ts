03660b8f70b263463734c7475dac0111438fd37f
chientarget <chienhi912@gmail.com>
2024-06-17 10:56:36 +0700

    Update

--- src/stores/config.ts
+++ src/stores/config.ts
@@ -1,12 +1,2 @@
-import { defineConfig } from '@vue/cli-service';
+export const API_URL = 'http://14.225.255.190:8081/api';
 
-export default defineConfig({
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://14.225.255.190:8081',
-                changeOrigin: true
-            }
-        }
-    }
-});
